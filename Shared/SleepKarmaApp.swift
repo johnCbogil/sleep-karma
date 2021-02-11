63fdf6872a980e1d91a616bb447f1392ef404938
John Bogil <johncbogil@gmail.com>
2021-02-11 13:57:53 -0500

    Initial Commit

---
+++ Shared/SleepKarmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepKarmaApp.swift
+//  Shared
+//
+//  Created by John Bogil on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SleepKarmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
